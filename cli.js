a958913e885297ae20598c76f9671b7fa08a2a3c
ASGdev <asg-dev@live.fr>
2022-11-09 00:39:10 -0500

    fix formatting + extended

--- cli.js
+++ cli.js
@@ -4,19 +4,20 @@ const fs = require('fs')
 const os = require('os')
 const checksum = require('checksum')
 const readdirp = require('readdirp')
-const util = require('util');
-const stream = require('stream');
+const util = require('util')
+const stream = require('stream')
+const process = require('process')
+const filenamify = require('filenamify')
 
 const streamFinished = util.promisify(stream.finished)
-
-const OUTPUT_FILE_DEFAULT_VALUE = "./sums.txt"
 
 const optionDefinitions = [
 	{ name: 'directory', alias: 'd', defaultOption: true },
 	{ name: 'algorithm', alias: 'a', type: String, defaultValue: "sha256" },
 	{ name: 'output', alias: 'o', type: Boolean, defaultValue: false },
-	{ name: 'file', alias: 'f', type: String, defaultValue: OUTPUT_FILE_DEFAULT_VALUE },
-	{ name: 'type', alias: 't', type: String, defaultValue: "tab" }
+	{ name: 'file', alias: 'f', type: String },
+	{ name: 'type', alias: 't', type: String, defaultValue: "tab" },
+	{ name: 'extended', alias: 'e', type: Boolean, defaultValue: false }
 ]
 const options = commandLineArgs(optionDefinitions);
 
@@ -31,8 +32,9 @@ const options = commandLineArgs(optionDefinitions);
 	
 	const isDir = fs.lstatSync(fpath).isDirectory();
 	
+
 	if(isDir){
-		for await (const entry of readdirp(options.directory)) {
+		for await (const entry of readdirp(options.directory, { alwaysStat: options.extended })) {
 			let hash = null
 			try {
 				hash = await computeHashPromise(entry.fullPath, options.algorithm)
@@ -40,20 +42,32 @@ const options = commandLineArgs(optionDefinitions);
 				console.log(e)
 			}
 
-			hashes.push({file: entry.path, hash: hash})
+			hashes.push({
+				file: entry.path, 
+				hash: hash, 
+				size: (entry.stats) ? entry.stats.size : null 
+			})
 		}
 
+	} else {
+		console.log("Must provide directory")
+		
+		process.exit(-1)
 	}
 
-	if(options.output){
+	if(options.file || options.output){
 		try {
-			const fname = options.file || OUTPUT_FILE_DEFAULT_VALUE
-			const file = fs.createWriteStream(fname, {encoding: 'utf8'})
+			const fname = options.file || filenamify(fpath + "-sums-" + Date.now())
+ 			const file = fs.createWriteStream(fname, {encoding: 'utf8'})
 	
 			switch (options.type) {
 				case 'tab':
 					for(couple of hashes){
-						file.write(couple.file + "\t" + couple.hash + "\r\n")
+						if (options.extended) {
+							file.write(couple.hash + "\t" + ((couple.size) ? couple.size + "\t" : "") + couple.file + "\r\n")
+						} else {
+							file.write(couple.hash + "\t" + couple.file + "\r\n")
+						}
 					}
 					file.end()
 					break;
@@ -70,8 +84,11 @@ const options = commandLineArgs(optionDefinitions);
 			process.exit(0)
 			
 		} catch(e){
-			console.log("Error writing file")
 			console.log(e)
+			
+			console.log("!!! error writing file")
+			
+
 			process.exit(-1)
 		}
 	} else {
